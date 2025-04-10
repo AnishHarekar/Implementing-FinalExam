033b93a49af2b25b263eccec48ba0adcb1ecb0aa
AnishHarekar <146278641+AnishHarekar@users.noreply.github.com>
2025-04-09 20:10:45 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/health')
 def home():
     return "Company: Wild Rydes | Developed By: Anish Harekar | ID: 100890955"
 
